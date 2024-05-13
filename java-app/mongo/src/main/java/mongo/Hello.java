6bb58dfbee055cd08706d16e502c798385b20516
pkredhat <120052595+pkredhat@users.noreply.github.com>
2024-05-13 10:46:56 -0400

    dumped demos

---
+++ java-app/mongo/src/main/java/mongo/Hello.java
@@ -0,0 +1,7 @@
+package mongo;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("sayHello");
+  }
+}
---
+++ java-app/mongo/src/main/java/mongo/db.java
@@ -0,0 +1,7 @@
+package mongo;
+
+public class db {
+    public static void main(String[] args) {
+        System.out.println("Connecting");
+    }
+}
